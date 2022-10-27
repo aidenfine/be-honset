db84fd86cf5f53cc4c349f588ae7324e241321f2
Aiden Fine <fineaiden@gmail.com>
2022-10-26 22:20:24 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Layout from '../components/layout'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <Layout>
+      <ToastContainer limit={2}/>
+        <Component {...pageProps} />
+    </Layout>
+     
+  )
 }
 
 export default MyApp
